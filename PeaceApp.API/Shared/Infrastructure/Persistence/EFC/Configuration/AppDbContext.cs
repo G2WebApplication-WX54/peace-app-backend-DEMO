4785feed1e35cb175a0e580df9d8ec57daf76d90
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-06-03 11:38:54 -0500

    chore: initial configuration

---
+++ PeaceApp.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,26 @@
+using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using Microsoft.EntityFrameworkCore;
+using PeaceApp.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+
+namespace PeaceApp.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        base.OnConfiguring(builder);
+        // Enable Audit Fields Interceptors
+        builder.AddCreatedUpdatedInterceptor();
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        // Place here you entity configurations
+        
+        // Apply SnakeCase Naming Convention
+        builder.UseSnakeCaseWithPluralizedTableNamingConvention();
+    }
+    
+}
